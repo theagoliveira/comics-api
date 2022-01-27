34decf53c6ffe72b61a0a39cb5a13c3340d2638c
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-27 14:34:15 -0300

    Initial commit

---
+++ src/main/java/br/com/zup/comicsapi/ComicsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.comicsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/comicsapi/ComicsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.comicsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComicsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
