f55401216a50e117820855e7b70b667ebbd14e17
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-27 21:14:27 -0300

    Create basic User class

---
+++ src/main/java/br/com/zup/comicsapi/models/User.java
@@ -0,0 +1,11 @@
+package br.com.zup.comicsapi.models;
+
+public class User {
+
+    private Long id;
+    private String name;
+    private String email;
+    private String cpf;
+    private String birthDate;
+
+}
