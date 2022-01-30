c174c3f2ae0c6c5b9a6e49901e851328af6034cf
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-29 23:00:43 -0300

    Add Feign client for Marvel API

--- src/main/java/br/com/zup/comicsapi/ComicsApiApplication.java
+++ src/main/java/br/com/zup/comicsapi/ComicsApiApplication.java
@@ -2,12 +2,14 @@ package br.com.zup.comicsapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ComicsApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ComicsApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ComicsApiApplication.class, args);
+    }
 
 }
---
+++ src/main/java/br/com/zup/comicsapi/feign/MarvelApiClient.java
@@ -0,0 +1,19 @@
+package br.com.zup.comicsapi.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.zup.comicsapi.marvel.MarvelObject;
+
+@FeignClient(value = "marvelApi", url = "https://gateway.marvel.com")
+public interface MarvelApiClient {
+
+    @GetMapping(value = "/v1/public/comics/{comicId}", produces = "application/json")
+    MarvelObject getComicByComicId(@PathVariable("comicId") Long comicId,
+                                   @RequestParam("ts") String ts,
+                                   @RequestParam("apikey") String apikey,
+                                   @RequestParam("hash") String hash);
+
+}
