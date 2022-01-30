3f914b5cee830daf51b176d42d30b17a5187f1e4
Thiago Cavalcante <thiago.kun@gmail.com>
2022-01-30 04:43:52 -0300

    Add findAll method to Comic Repository

--- src/main/java/br/com/zup/comicsapi/repositories/ComicRepository.java
+++ src/main/java/br/com/zup/comicsapi/repositories/ComicRepository.java
@@ -1,5 +1,7 @@
 package br.com.zup.comicsapi.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,9 @@ import br.com.zup.comicsapi.models.Comic;
 
 @Repository
 public interface ComicRepository extends CrudRepository<Comic, Long> {
+
+    @Override
+    List<Comic> findAll();
 
     Boolean existsByIsbn(String isbn);
 
